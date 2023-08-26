56cd6ce7e846e7e7cdd92d06ac0d15bed613c15d
Иван-Т0 <vanyataran24@gmail.com>
2023-08-26 20:28:02 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { useState } from "react";
+import Searchbar from "./Searchbar/Searchbar";
+import ImageGallery from "./ImageGallery/ImageGallery";
+
+const App = ()=>{
+  const [query, setQuery] = useState('');
+  // const [images, setImages] = useState('')
+
+
+//  const handleInput = (e) => {
+//     e.preventDefault();
+//     const { value } = e.target;
+   
+//        setQuery(value);
+    
+//   };
+
+  const handleSubmit = (query) => {
+    setQuery(query);
+  };
+
+ 
+    return (
+      <div>
+        <Searchbar onSubmit={handleSubmit} value={query} />
+        <ImageGallery query={query}  />
+      </div>
+    );
+  
+}
+export default App;
